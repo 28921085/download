937ce8f11c97cb9f62c966217562f65c957ba581
28921085 <83084203+28921085@users.noreply.github.com>
2021-11-26 11:18:58 +0800

    Add files via upload

---
+++ HW5.php
@@ -0,0 +1,14 @@
+<form action="URL" method ="POST/GET">
+<input type="text" name="參數1">&
+<input type="hidden" name="參數2">&
+<input type="password" name="參數3">&
+<select name="參數4">
+<text area name="參數5">
+<input type="checkbox" name="參數6">&
+<input type="radio" name="參數7">&
+<input type="submit"> &
+</form>
+<?php 
+    echo "78456123879465123"<br>
+    echo "888888888888"<br>
+?>
